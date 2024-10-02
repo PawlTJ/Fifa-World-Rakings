7361848e9c9b427eef1a8a436ecffdb5c5e48259
PawlTJ <pawltj@hotmail.com>
2024-10-01 23:56:27 -0400

    adding comments for review

--- rankings_check.py
+++ rankings_check.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import tkinter as tk
+import pandas as pd                 # Pandas is used to manipulate data 
+import matplotlib.pyplot as plt     # Matplotlib library is use to create graphs
+import tkinter as tk                # Tkinter creates the window (applacation)
 from tkinter import ttk
 
-df = pd.read_csv('ranking_fifa.csv')
-df['Year'] = df['date'].str[:4].astype(int)
+df = pd.read_csv('ranking_fifa.csv')            # Reads CSV file in the same folder as the program
+df['Year'] = df['date'].str[:4].astype(int)     # Taking the Date column and extract the Year and save as an Integer in a new column
 
-average_points_per_team_per_year = df.groupby(['Year', 'team'])['total_points'].mean().reset_index() # Calculate the average points per year for each team
-teams = sorted(average_points_per_team_per_year['team'].unique()) # Get a list of all unique teams
+average_points_per_team_per_year = df.groupby(['Year', 'team'])['total_points'].mean().reset_index()    # Calculate the average points per year for each team
+teams = sorted(average_points_per_team_per_year['team'].unique())                                       # Get a list of all unique teams
 
 # Create tkinker window
 root = tk.Tk()
@@ -15,6 +15,7 @@ root.title("FIFA Team Comparison")
 
 root.geometry("400x200")
 
+# Create Labels
 team_1_label = tk.Label(root, text="Select First Team:")
 team_1_label.pack(pady=5)
 
@@ -30,11 +31,13 @@ team_2_dropdown = ttk.Combobox(root, textvariable=team_2_var, values=teams)
 team_2_dropdown.pack(pady=5)
 
 
+
+# Create the graph using the inputs from Labels / plot_teams() is a function create that we can call again
 def plot_teams():
     team_1_name = team_1_var.get()
     team_2_name = team_2_var.get()
 
-    if team_1_name and team_2_name:
+    if team_1_name and team_2_name:     # If both are True (exist) contiune 
         team_1_data = average_points_per_team_per_year[average_points_per_team_per_year['team'] == team_1_name]
         team_2_data = average_points_per_team_per_year[average_points_per_team_per_year['team'] == team_2_name]
 
@@ -52,9 +55,9 @@ def plot_teams():
 
         plt.show()
     else:
-        tk.messagebox.showerror("Input Error", "Please select both teams.")
+        tk.messagebox.showerror("Input Error", "Please select both teams.") # If a team is missing, show this message 
 
-plot_button = tk.Button(root, text="Plot", command=plot_teams)
+plot_button = tk.Button(root, text="Plot", command=plot_teams) # On clicking button, run plot_teams function
 plot_button.pack(pady=20)
 
-root.mainloop()
+root.mainloop()  # run root (main program)
